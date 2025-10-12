c31f32dbbc7aa93463a8e380b16666cc61321088
Earnest-Williams <Earnest.Williams@gmail.com>
2025-10-12 01:04:59 +0100

    Align order polarity opposition

--- src/combat/constants.js
+++ src/combat/constants.js
@@ -2,10 +2,10 @@
 // @ts-check
 
 export const POLAR_BIAS = {
-  order: { chaos: 0.1, void: -0.05 },
+  order: { chaos: 0.1, decay: 0.1 },
   growth: { decay: 0.1, void: -0.05 },
   chaos: { order: 0.1, void: -0.05 },
-  decay: { growth: 0.1, void: -0.05 },
+  decay: { growth: 0.1, order: 0.1, void: -0.05 },
   void: { order: -0.05, growth: -0.05, chaos: -0.05, decay: -0.05 },
 };
 
